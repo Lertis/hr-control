0b13e5987a595a9ff507d1cca8de43199d02c8fc
Yura Husak <yura.gusak7@gmail.com>
2021-05-26 09:41:33 +0300

    table with legend added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TableWithLegend from './components/table.with.legend';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	render() {
+		const list = [
+			{ name: "Email", info: "yura.gusak7@gmail.com" },
+			{ name: "Work mobile", info: "+380678465227" },
+			{ name: "Skype", info: "paradise8538" },
+			{ name: "Status", info: "Current" },
+			{ name: "Last Login", info: "25/05/2021 19:04" }
+		];
+		return (
+			<TableWithLegend legend="Legend" tableItems={list} />
+		);
+	}
 }
 
 export default App;
---
+++ src/components/table.with.legend.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./table.with.legend.css";
+
+class TableWithLegend extends React.Component {
+	render() {
+		return (
+			<fieldset>
+				<legend>{this.props.legend || "Table Legend"}</legend>
+				<table>
+					<tbody>
+						{this.props.tableItems.map((el, i) =>
+							<React.Fragment>
+								<tr key={i}>
+									<th><strong>{el.name}</strong></th>
+									<td><div>{el.info}</div></td>
+								</tr>
+							</React.Fragment>
+						)}
+					</tbody></table>
+			</fieldset>
+		);
+	}
+}
+
+export default TableWithLegend;
